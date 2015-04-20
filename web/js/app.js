d7f5438a880a8c5d28106a5a4fd8e4108d9e6ab9
Cristian Campos <campos@strands.com>
2015-04-20 15:42:40 -0300

    Product binding

--- web/js/app.js
+++ web/js/app.js
@@ -1,7 +1,8 @@
 angular.module('Strands Demo', [])
-  .controller('ProductsController', function($http, $scope, $location) {
-    $http.get("/data/products.json").success(function(response) {
-      var products = JSON.parse(products);
-      $scope.product = product[1];
+  .controller('ProductsController', ['$http', '$scope','$location',function($http, $scope, $location) {
+    $http.get("/data/products.json").success(function(products) {
+      var url = $location.absUrl();
+      var id = parseInt(url.slice(url.indexOf("id")+3));
+      $scope.product = products[id];
     });
-  });
+  }]);
