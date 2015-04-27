b19b1a89b6cbc296be7887f51050c5c09d0934b4
Cristian Campos <campos@strands.com>
2015-04-27 16:27:20 -0300

    Cart commited

--- web/js/app.js
+++ web/js/app.js
@@ -1,23 +1,58 @@
-angular.module('Strands Demo', [])
-  .controller('ProductsController', ['$http', '$scope','$location',function($http, $scope, $location) {
-    $http.get("/data/products.json").success(function(products) {
-      var url = $location.absUrl();
-      var id = parseInt(url.slice(url.indexOf("id")+3));
-      $scope.product = products[id-1];
+app = angular.module('Strands Demo', ['ngCart']);
+app.controller('ProductsController', ['$http', '$scope', '$location', function($http, $scope, $location) {
+  $http.get("/data/products.json").success(function(products) {
+    var url = $location.absUrl();
+    var id = parseInt(url.slice(url.indexOf("id") + 3));
+    $scope.product = products[id - 1];
 
-      
-      try{
-  		//Event Definitions
-  		//Make sure first that the array is defined
-  		if (typeof StrandsTrack=="undefined"){StrandsTrack=[];}
-  		//Item was visisted
-  			StrandsTrack.push({
-		    event:"visited",
-    		item: id
-  		});
 
-  		//Launch processing
-		SBS.Worker.go("0eJkBiACnt");
-	  } catch (e){};
-    });
-  }]);
+    try {
+      //Event Definitions
+      //Make sure first that the array is defined
+      if (typeof StrandsTrack == "undefined") {
+        StrandsTrack = [];
+      }
+      //Item was visisted
+      StrandsTrack.push({
+        event: "visited",
+        item: id
+      });
+
+      //Launch processing
+      SBS.Worker.go("0eJkBiACnt");
+    } catch (e) {}
+  });
+}]);
+app.controller('CheckoutController', ['$http', '$scope', 'ngCart', function($http, $scope, ngCart) {
+
+  SBS.Event.subscribe("event.presubmit", function(evt) {
+    try {
+        if (evt.event == 'purchase') {
+          evt.item = evt.item.slice(0,evt.item.indexOf("-"));
+        }
+    } catch (eve) {
+      SBS.alert('p1', eve);
+    }
+    return true;
+  });
+
+  cart = ngCart.toObject();
+  $scope.checkout = function() {
+    try {
+      //Event Definitions
+      //Make sure first that the array is defined
+      if (typeof StrandsTrack == "undefined") {
+        StrandsTrack = [];
+      }
+      angular.forEach(cart.items, function(value, key) {
+        StrandsTrack.push({
+          event: "purchase",
+          item: value.id
+        });
+      });
+
+      //Launch processing
+      SBS.Worker.go("0eJkBiACnt");
+    } catch (e) {}
+  };
+}]);
